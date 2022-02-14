a20669ffcdf3f91f4cbaf75204a575931d605a7c
Ethan Brown <brownev19@gcc.edu>
2022-02-13 21:31:49 -0500

    comments

--- shell.c
+++ shell.c
@@ -1,10 +1,11 @@
 /*
-Author: Ethan Brown
-Course: COMP 340, Operating Systems
-Date: 13 February 2022
-Description: This file implements the Shell program
-Compile with: gcc -o shell shell.c
-run with: ./shell
+* Author:       Ethan Brown
+* Course:       COMP 340, Operating Systems
+* Date:         13 February 2022
+* Description:  This file implements the 
+*               Shell program
+* Compile with: gcc -o shell shell.c
+* Run with:     ./shell
 */
 #include<stdio.h>
 #include<string.h>
@@ -56,12 +57,14 @@ int shell_execute(char *file_path, char **argv) {
 		if((status = execv(file_path, argv)) == -1);
 			exit(status);
 	} else {
+		//parent
 		wait(NULL);
 	}
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
+	///run the shell
 	char buf[1024];
 	char cwd[1024];
 
@@ -72,9 +75,23 @@ int main(int argc, char *argv[]) {
 		printf("%s$ ", cwd);
 		fscanf(stdin, "%s", buf);
 		puts(buf);
-		
+
+		//2. filter out whitespace command 
+
+    //3. if command line contains non-whitespace characters
+
+		//if the specified command is “exit”, terminate the program taking care to release any allocated resources.
 		if(strcmp(buf, "exit")) {
 			exit = 1;
 		}
+    //if the specified command is “cd”
+
+    //  change the current working directory to the specified directory path using shell_change_dir()
+    // if the command is specified using an absolute path, exists in the user’s PATH or exists in the current folder
+			// execute the command using shell_execute()
+		// else
+			// report an error message
+
+
 	}
 }
